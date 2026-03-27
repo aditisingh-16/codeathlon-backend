ed02311011b9b4f1234762d2a3476f18e7718a92
Aditi Singh <btbtc24201_aditi@banasthali.in>
2026-03-27 08:59:07 +0530

    Added streak system with daily tracking logic

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,26 @@
 import java.util.List;
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
 
+        // Daily workout
         List<Problem> problems = ProblemRepository.getBalancedWorkout("Amazon");
 
+        System.out.println("Today's Workout:");
         for (Problem p : problems) {
-            System.out.println(p.getTitle() + " | " + p.getTopic() + " | " + p.getDifficulty() + " | " + p.getCompany());
+            System.out.println(p.getTitle() + " | " + p.getTopic() + " | " + p.getDifficulty());
         }
+
+        // Streak simulation
+       UserStats user = new UserStats();
+
+        // Simulate yesterday activity
+        user.setLastSolvedDate(java.time.LocalDate.now().minusDays(1));
+        user.setStreak(1); // important!
+
+        user.updateStreak(); // today
+
+        System.out.println("\nCurrent Streak: " + user.getStreak());
     }
 }
---
+++ src/UserStats.java
@@ -0,0 +1,38 @@
+import java.time.LocalDate;
+
+public class UserStats {
+
+    private int streak;
+    private LocalDate lastSolvedDate;
+
+    public UserStats() {
+        this.streak = 0;
+        this.lastSolvedDate = null;
+    }
+
+    public void updateStreak() {
+        LocalDate today = LocalDate.now();
+
+        if (lastSolvedDate == null) {
+            streak = 1;
+        } else if (lastSolvedDate.plusDays(1).equals(today)) {
+            streak++;
+        } else if (lastSolvedDate.equals(today)) {
+            // same day, no change
+        } else {
+            streak = 1;
+        }
+
+        lastSolvedDate = today;
+    }
+
+    public int getStreak() {
+        return streak;
+    }
+    public void setLastSolvedDate(LocalDate date) {
+        this.lastSolvedDate = date;
+    }
+    public void setStreak(int streak) {
+            this.streak = streak;
+    }
+}
